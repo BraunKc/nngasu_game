9926e318ad07b0ede39a30dc0ed77c65ab8b1004
BraunKc <152529764+BraunKc@users.noreply.github.com>
2024-01-29 14:05:15 +0300

    Add files via upload

---
+++ anime.js
@@ -0,0 +1,23 @@
+const btn = document.getElementById('btn');
+var colors = ['#000', '#fff', '#008000', '#ffd700']
+
+
+
+btn.addEventListener('mouseenter', () => {
+    function getRandomColor() {
+        var randomIndex = Math.floor(Math.random() * colors.length);
+        return colors[randomIndex];
+      }
+      
+      // Получаем случайный цвет
+      var randomColor = getRandomColor();
+
+anime({
+    targets: btn,
+    backgroundColor: randomColor,
+    translateX: () => Math.random() * 250,
+    translateY: () => Math.random() * 250,
+    easing: "easeOutQuint",
+    duration: 500
+  })
+})
